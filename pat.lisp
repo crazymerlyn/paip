a2b6cdff8906a243779827f177f2de01869e82f8
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2018-05-10 18:48:34 +0530

    Create a pattern matcher

---
+++ pat.lisp
@@ -0,0 +1,43 @@
+(defun variable-p (x)
+  "Is x a variable (a symbol beginning with '?')?"
+  (and (symbolp x) (equal (char (symbol-name x) 0) #\?)))
+
+(defconstant fail nil "Indicates pat-match failure")
+(defparameter no-bindings '((t . t))
+             "Indicates pat-match success with no variables.")
+
+(defun get-binding (var bindings)
+  "Find a (variable . value) pair in a binding list."
+  (assoc var bindings))
+
+(defun binding-val (binding)
+  "Get the value part of the binding"
+  (cdr binding))
+
+(defun extend-bindings (var val bindings)
+  "Add a (var . value) pair to a bindings list"
+  (cons (cons var val) bindings))
+
+(defun lookup (var bindings)
+  "Get the value part (for var) from a binding list."
+  (binding-val (get-binding var bindings)))
+
+(defun pat-match (pattern input &optional (bindings no-bindings))
+  "Match pattern against input in the context of the bindings"
+  (cond ((eq bindings fail) fail)
+        ((variable-p pattern)
+         (match-variable pattern input bindings))
+        ((eql pattern input) bindings)
+        ((and (consp pattern) (consp input))
+         (pat-match (rest pattern) (rest input)
+                    (pat-match (first pattern) (first input)
+                               bindings)))
+        (t fail)))
+
+(defun match-variable (var input bindings)
+  "Does VAR match input? Uses (or updates) and returns bindings."
+  (let ((binding (get-binding var bindings)))
+    (cond ((not binding) (extend-bindings var input bindings))
+          ((equal input (binding-val binding)) bindings)
+          (t fail))))
+
