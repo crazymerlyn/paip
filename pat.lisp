7f13ad6db2a5a3ac70ee2259bfdc4c8259cd8349
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2018-05-11 16:54:50 +0530

    Implement pat-match with segment matching

--- pat.lisp
+++ pat.lisp
@@ -1,3 +1,5 @@
+(load "util.lisp")
+
 (defun variable-p (x)
   "Is x a variable (a symbol beginning with '?')?"
   (and (symbolp x) (equal (char (symbol-name x) 0) #\?)))
@@ -28,11 +30,35 @@
         ((variable-p pattern)
          (match-variable pattern input bindings))
         ((eql pattern input) bindings)
+        ((segment-pattern-p pattern)
+         (segment-match pattern input bindings))
         ((and (consp pattern) (consp input))
          (pat-match (rest pattern) (rest input)
                     (pat-match (first pattern) (first input)
                                bindings)))
         (t fail)))
+
+(defun segment-pattern-p (pattern)
+  "Is this a segment matching pattern: ((?* var) . pat)"
+  (and (consp pattern)
+       (starts-with (first pattern) '?*)))
+
+(defun segment-match (pattern input bindings &optional (start 0))
+  "Match the segment pattern ((?* var) . pat) against input"
+  (let ((var (second (first pattern)))
+        (pat (rest pattern)))
+    (if (null pat)
+        (match-variable var input bindings)
+        ;; We assume that pat starts with a constant
+        ;; In other words, a pattern can't have two consecutive vars
+        (let ((pos (position (first pat) input
+                             :start start :test #'equal)))
+          (if (null pos)
+              fail
+              (let ((b2 (pat-match pat (subseq input pos) bindings)))
+                (if (eq b2 fail)
+                    (segment-match pattern input bindings (+ pos 1))
+                    (match-variable var (subseq input 0 pos) b2))))))))
 
 (defun match-variable (var input bindings)
   "Does VAR match input? Uses (or updates) and returns bindings."
