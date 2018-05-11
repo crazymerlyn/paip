7a5e17cffb7c4b029fd730bb8535160f3f104752
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2018-05-11 19:41:32 +0530

    Implement eliza

---
+++ eliza.lisp
@@ -0,0 +1,65 @@
+(load "pat.lisp")
+
+(defun rule-pattern (rule) (first rule))
+(defun rule-responses (rule) (rest rule))
+
+(defparameter *eliza-rules*
+  '((((?* ?x) hello (?* ?y))
+     (How do you do. Please state your problem.))
+    (((?* ?x) I want (?* ?y))
+     (What would it mean if you got ?y)
+     (Why do you want ?y) (Suppose you got ?y soon))
+    (((?* ?x) if (?* ?y))
+     (Do you really think its likely that ?y)
+     (Do you wish that ?y)
+     (What do you think about ?y)
+     (Really-- if ?y))
+    (((?* ?x) no (?* ?y))
+     (Why not?) (You are being a bit negative)
+     (Are you saying "NO" just to be negative?))
+    (((?* ?x) I was (?* ?y))
+     (Were you really?) (Perhaps I already knew you were ?y)
+     (Why? do you tell me you were ?y now?))
+    (((?* ?x) I feel (?* ?y))
+     (Do you often feel ?y ?))
+    (((?* ?x) I felt (?* ?y))
+     (What other feelings do you have?))))
+
+
+(defun eliza ()
+  "Respond to user input using pattern matching rules."
+  (loop
+    (print 'eliza>)
+    (write (flatten (use-eliza-rules (read))) :pretty t)))
+
+(defun use-eliza-rules (input)
+  "Find some rule with which to transform input."
+  (some #'(lambda (rule)
+            (let ((result (pat-match (rule-pattern rule) input)))
+              (if (not (eq result fail))
+                  (sublis (switch-viewpoint result)
+                          (random-elt (rule-responses rule))))))
+        *eliza-rules*))
+
+(defun switch-viewpoint (words)
+  "Change I to you and vice versa, and so on."
+  (sublis '((I . you) (you . I) (me . you) (am . are))
+          words))
+
+(defun flatten (the-list)
+  "Append together elements (or lists) in the list"
+  (mappend #'mklist the-list))
+
+(defun mklist (x)
+  "Return x if it is a list else (x)."
+  (if (listp x)
+      x
+      (list x)))
+
+(defun mappend (fn the-list)
+  "Apply fn to each element of the list and append the results."
+  (apply #'append (mapcar fn the-list)))
+
+(defun random-elt (choices)
+  "Choose an element from a list at random."
+  (elt choices (random (length choices))))
