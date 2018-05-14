d57fe396a6a01748684d7cee6411670b63718790
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2018-05-14 15:13:06 +0530

    Extend pat-match

--- pat.lisp
+++ pat.lisp
@@ -12,6 +12,10 @@
   "Find a (variable . value) pair in a binding list."
   (assoc var bindings))
 
+(defun binding-var (binding)
+  "Get the variable part of the binding"
+  (car binding))
+
 (defun binding-val (binding)
   "Get the value part of the binding"
   (cdr binding))
@@ -31,17 +35,92 @@
          (match-variable pattern input bindings))
         ((eql pattern input) bindings)
         ((segment-pattern-p pattern)
-         (segment-match pattern input bindings))
+         (segment-matcher pattern input bindings))
+        ((single-pattern-p pattern)
+         (single-matcher pattern input bindings))
         ((and (consp pattern) (consp input))
          (pat-match (rest pattern) (rest input)
                     (pat-match (first pattern) (first input)
                                bindings)))
         (t fail)))
 
+(setf (get '?is 'single-match) 'match-is)
+(setf (get '?or 'single-match) 'match-or)
+(setf (get '?and 'single-match) 'match-and)
+(setf (get '?not 'single-match) 'match-not)
+
+(setf (get '?* 'segment-match) 'segment-match)
+(setf (get '?+ 'segment-match) 'segment-match+)
+(setf (get '?? 'segment-match) 'segment-match?)
+(setf (get '?if 'segment-match) 'match-if)
+
 (defun segment-pattern-p (pattern)
-  "Is this a segment matching pattern: ((?* var) . pat)"
+  "Is this a segment matching pattern like ((?* var) . pat)?"
   (and (consp pattern)
-       (starts-with (first pattern) '?*)))
+       (consp (first pattern))
+       (symbolp (first (first pattern)))
+       (segment-match-fn (first (first pattern)))))
+
+(defun single-pattern-p (pattern)
+  "Is this a single-matching pattern?
+   E.g. (?is x predicate) (?and . patterns) (?or . patterns)."
+  (and (consp pattern)
+       (single-match-fn (first pattern))))
+
+(defun segment-matcher (pattern input bindings)
+  "Call the right function for this kind of segment pattern."
+  (funcall (segment-match-fn (first (first pattern)))
+           pattern input bindings))
+
+(defun single-matcher (pattern input bindings)
+  "Call the right function for this kind of single pattern."
+  (funcall (single-match-fn (first pattern))
+           (rest pattern) input bindings))
+
+(defun segment-match-fn (x)
+  "Get the segment match function for x,
+   if it is a symbol that has one."
+  (when (symbolp x) (get x 'segment-match)))
+
+(defun single-match-fn (x)
+  "Get the single match function for x,
+   if it is a symbol that has one."
+  (when (symbolp x) (get x 'single-match)))
+
+(defun match-is (var-and-pred input bindings)
+  "Succeed and bind var if the input satisfies pred,
+   where var-and-pred is the list (var pred)."
+  (let* ((var (first var-and-pred))
+         (pred (second var-and-pred))
+         (new-bindings (pat-match var input bindings)))
+    (if (or (eq new-bindings fail)
+            (not (funcall pred input)))
+        fail
+        new-bindings)))
+
+(defun match-and (patterns input bindings)
+  "Succeed if all of the patterns match the input."
+  (cond ((eq bindings fail) fail)
+        ((null patterns) bindings)
+        (t (match-and (rest patterns) input
+                      (pat-match (first patterns) input
+                                 bindings)))))
+
+(defun match-or (patterns input bindings)
+  "Succeed if any of the patterns match the input."
+  (if (null patterns)
+      fail
+      (let ((new-bindings (pat-match (first patterns) input bindings)))
+        (if (eq new-bindings fail)
+            (match-or (rest patterns) input bindings)
+            new-bindings))))
+
+(defun match-not (patterns input bindings)
+  "Succeed if none of the patterns match the input.
+   This will never bind any variables."
+  (if (eq (match-or patterns input bindings) fail)
+      bindings
+      fail))
 
 (defun segment-match (pattern input bindings &optional (start 0))
   "Match the segment pattern ((?* var) . pat) against input"
@@ -49,10 +128,7 @@
         (pat (rest pattern)))
     (if (null pat)
         (match-variable var input bindings)
-        ;; We assume that pat starts with a constant
-        ;; In other words, a pattern can't have two consecutive vars
-        (let ((pos (position (first pat) input
-                             :start start :test #'equal)))
+        (let ((pos (first-match-pos (first pat) input start)))
           (if (null pos)
               fail
               (let ((b2 (pat-match
@@ -62,10 +138,36 @@
                     (segment-match pattern input bindings (+ pos 1))
                     b2)))))))
 
+(defun segment-match+ (pattern input bindings)
+  "Match one or more elements of input."
+  (segment-match pattern input bindings 1))
+
+(defun segment-match? (pattern input bindings)
+  "Match zero or one element of input."
+  (let ((var (second (first pattern)))
+        (pat (rest pattern)))
+    (or (pat-match (cons var pat) input bindings)
+        (pat-match pat input (extend-bindings var nil bindings)))))
+
+(defun match-if (pattern input bindings)
+  "Test an arbitrary expression involving variables.
+   The pattern look like ((if? code) . rest)."
+  (if (eval (sublis bindings (second (first pattern))))
+      (pat-match (rest pattern) input bindings)
+      fail))
+
+(defun first-match-pos (pat1 input start)
+  "Find the first position that pat1 could possibly match input,
+   starting at position start. If pat1 is non-constant, the just
+   return start."
+  (cond ((and (atom pat1) (not (variable-p pat1)))
+         (position pat1 input :start start :test #'equal))
+        ((< start (length input)) start)
+        (t nil)))
+
 (defun match-variable (var input bindings)
   "Does VAR match input? Uses (or updates) and returns bindings."
   (let ((binding (get-binding var bindings)))
     (cond ((not binding) (extend-bindings var input bindings))
           ((equal input (binding-val binding)) bindings)
           (t fail))))
-
